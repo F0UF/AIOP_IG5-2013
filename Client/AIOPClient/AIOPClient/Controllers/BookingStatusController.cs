3d5b85d4c616a23599d728048ade6555a9f8b57a
Shlipack <ludwigforte@live.fr>
2013-11-17 13:12:28 +0100

    Avancement sur le controlleur bookingstatus avec la désérialisation du json, ne marche pas pour le moment

--- Client/AIOPClient/AIOPClient/Controllers/BookingStatusController.cs
+++ Client/AIOPClient/AIOPClient/Controllers/BookingStatusController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using AIOPClient.Models;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace AIOPClient.Controllers
 {
@@ -13,10 +18,41 @@ namespace AIOPClient.Controllers
 
         public ActionResult Index()
         {
+            getReservationList();
             return View();
         }
 
-        public getReservationList
+        public bool getReservationList()
+        {
+            statusListModel model = new statusListModel();
+            using (var client = new WebClient())
+            {
+                var json = client.DownloadString("http://aiopninjaserver.no-ip.biz/api/planning/display?id_teacher=11");
 
+                if (json.Equals(""))
+                {
+                    return false;
+                }
+                else
+                {
+                    JObject o = JObject.Parse(json);
+                    model.dateBegin = (string)o["Start_Date"];
+                    model.dateEnd = (string)o["End_Date"];
+                    model.teaching = (string)o["Module_Name"];
+                    model.promotion = (string)o["Group_Name"];
+                    model.computers = (string)o["Computer"];
+                    model.projector = (string)o["Projector"];
+                    model.status = (string)o["State"];
+                    Console.WriteLine(model.dateBegin);
+                    Console.WriteLine(model.dateEnd);
+                    Console.WriteLine(model.teaching);
+                    Console.WriteLine(model.promotion);
+                    Console.WriteLine(model.computers);
+                    Console.WriteLine(model.projector);
+                    Console.WriteLine(model.status);
+                    return true;
+                }
+            }
+        }
     }
 }
---
+++ Client/AIOPClient/AIOPClient/Models/statusListModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AIOPClient.Models
+{
+    public class statusListModel
+    {
+        public int Id { get; set; }
+        public String dateBegin { get; set; }
+        public String dateEnd { get; set; }
+        public String teaching { get; set; }
+        public String promotion { get; set; }
+        public String computers { get; set; }
+        public String projector { get; set; }
+        public String status { get; set; }
+    }
+}
