4e3f9a3c97d84491ad7956bd49668489193e7579
Tom <tkuhnen34@gmail.com>
2013-11-17 23:06:21 +0100

    Customization MyCalendar

--- Client/AIOPClient/AIOPClient/Controllers/MyCalendarController.cs
+++ Client/AIOPClient/AIOPClient/Controllers/MyCalendarController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,5 +17,27 @@ namespace AIOPClient.Controllers
             return View();
         }
 
+        [ActionName("GetEvents")]
+        public JArray GetEvents(double start, double end)
+        {
+
+            JArray ja = new JArray();
+
+            JObject jo = new JObject();
+            jo.Add("title", "Objective-C");
+            jo.Add("start", "2013-12-19 15:30:00");
+            jo.Add("end", "2013-12-19 17:30:00");
+            jo.Add("allDay", false);
+            ja.Add(jo);
+
+            jo = new JObject();
+            jo.Add("title", "Reseaux");
+            jo.Add("start", "2013-12-18 12:30:00");
+            jo.Add("end", "2013-12-18 14:30:00");
+            jo.Add("allDay", false);
+            ja.Add(jo);
+
+            return ja ;
+        }
     }
 }
