b05bda5979285f7218bb2dabc8f658a7a5014a2f
Vincent Phulpin <phulpin.vincent@gmail.com>
2013-11-17 17:36:09 +0100

    notion de session + package

---
+++ Client/AIOPClient/AIOPClient/Models/UserSession.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AIOPClient.Models
+{
+    public class UserSession
+    {
+        // Variable locale pour stocker une référence vers l'instance
+	    private static UserSession instance = null;
+	    private static readonly object mylock = new object();
+        public bool logged { get; set; }
+        public String userName { get; set; }
+
+	    private UserSession()
+	    {
+            logged = false;
+	    }
+
+	    // La méthode GetInstance doit être Shared
+	    public static UserSession GetInstance()
+	    {
+
+		    lock ((mylock)) {
+			    // Si pas d'instance existante on en crée une...
+			    if (instance == null)
+			    {
+				    instance = new UserSession();
+			    }
+
+			    // On retourne l'instance de MonSingleton
+			    return instance;
+		    }
+	    }
+    }
+}
