e5dfea2160b91bf25dd41c676b67cdfb2dbdef04
Tom <tkuhnen34@gmail.com>
2013-11-18 08:59:35 +0100

    packages maquants

--- Client/AIOPClient/AIOPClient/Controllers/MyCalendarController.cs
+++ Client/AIOPClient/AIOPClient/Controllers/MyCalendarController.cs
@@ -18,26 +18,34 @@ namespace AIOPClient.Controllers
         }
 
         [ActionName("GetEvents")]
-        public JArray GetEvents(double start, double end)
+        public ActionResult GetEvents()
         {
 
-            JArray ja = new JArray();
+            //JArray ja = new JArray();
 
-            JObject jo = new JObject();
-            jo.Add("title", "Objective-C");
-            jo.Add("start", "2013-12-19 15:30:00");
-            jo.Add("end", "2013-12-19 17:30:00");
-            jo.Add("allDay", false);
-            ja.Add(jo);
+            //JObject jo = new JObject();
+            //jo.Add("title", "Objective-C");
+            //jo.Add("start", "2013-12-19 15:30:00");
+            //jo.Add("end", "2013-12-19 17:30:00");
+            //jo.Add("allDay", false);
+            //ja.Add(jo);
 
-            jo = new JObject();
-            jo.Add("title", "Reseaux");
-            jo.Add("start", "2013-12-18 12:30:00");
-            jo.Add("end", "2013-12-18 14:30:00");
-            jo.Add("allDay", false);
-            ja.Add(jo);
+            //jo = new JObject();
+            //jo.Add("title", "Reseaux");
+            //jo.Add("start", "2013-12-18 12:30:00");
+            //jo.Add("end", "2013-12-18 14:30:00");
+            //jo.Add("allDay", false);
+            //ja.Add(jo);
 
-            return ja ;
+            //return ja ;
+
+            var events = new[]
+        {     
+            new { title = "test", start = "2013-11-19T14:08:00Z", end = "2013-11-19T16:09:00Z", editable = false },
+            new { title = "test", start = "2013-11-19 16:08:00 ", end = "2013-11-19 18:09:00", editable = false },
+            new { title = "test", start = "2013-11-20", end = "2013-11-20", editable = false }
+        };
+            return Json(events, JsonRequestBehavior.AllowGet);
         }
     }
 }
