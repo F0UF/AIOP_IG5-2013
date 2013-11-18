700772bf311a42198c68a49f2da57922c0e921c4
Shlipack <ludwigforte@live.fr>
2013-11-18 16:18:38 +0100

    ManageBooking controlleur fonctionnel, manque validation/refus

--- Client/AIOPClient/AIOPClient/Controllers/ManageBookingController.cs
+++ Client/AIOPClient/AIOPClient/Controllers/ManageBookingController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,8 +16,40 @@ namespace AIOPClient.Controllers
 
         public ActionResult Index()
         {
+            getWaitingList();
             return View();
         }
 
+        public bool getWaitingList()
+        {
+            ////Api's url building
+            String urlApi = "http://aiopninjaserver.no-ip.biz/api/admin/waiting";
+            //Deserialization of the returned json
+            using (var client = new WebClient())
+            {
+                var result = client.DownloadString(urlApi);
+                JArray jsonVal = JArray.Parse(result) as JArray;
+                dynamic waitings = jsonVal;
+                ViewBag.waitingsList = waitings;
+                return true;
+            }
+        }
+
+        [HttpPost]
+        public bool refuseReservation(int id)
+        {
+            Debug.WriteLine(id);
+            //Api's url building
+            String urlApi = "http://aiopninjaserver.no-ip.biz/api/admin/Refuse?Id_Booking=" + id.ToString();
+
+            using (var client = new WebClient())
+            {
+                var result = client.DownloadString(urlApi);
+                //If return is null then the reservations wasn't refused properly
+                if (result == null)
+                    return false;
+            }
+            return true;
+        }
     }
 }
