c7ac6a05597a3ff56f54a89448c8a7cbff09813b
YaTro <simon_barrat@hotmail.fr>
2013-11-18 10:23:38 +0100

    CreateTeaching

---
+++ Client/AIOPClient/AIOPClient/Controllers/CreateTeachingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AIOPClient.Controllers
+{
+    public class CreateTeachingController : Controller
+    {
+        //
+        // GET: /CreateTeaching/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
