4857cf99b628ef850f87ff930b374a90e461ac69
Tom <tkuhnen34@gmail.com>
2013-11-20 02:59:03 +0100

    Modification message d'erreur

--- Client/AIOPClient/AIOPClient/Models/UserModel.cs
+++ Client/AIOPClient/AIOPClient/Models/UserModel.cs
@@ -13,11 +13,11 @@ namespace AIOPClient.Models
 
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Username is required.")]
         [Display(Name = "Username")]
         public string Username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password is required.")]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
